8f88992859a2bb64794b37debbfec9291c5fa51e
Steven <Ceglareks954@gmail.com>
2023-06-23 17:11:50 -0400

    Initial Commit

---
+++ Controllers/DojoSurveyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithValidationsNew.Models;
+
+namespace DojoSurveyWithValidationsNew.Controllers
+{
+	public class DojoSurveyController : Controller
+	{
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("process")]
+        public IActionResult Process(SurveyModel surveyModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Results", surveyModel);
+            }
+
+            return View("Index");
+        }
+
+        [HttpGet("results")]
+        public IActionResult Results(SurveyModel surveyModel)
+        {
+            return View("Results", surveyModel);
+        }
+    }
+}
+
---
+++ Models/SurveyModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DojoSurveyWithValidationsNew.Models
+{
+	public class SurveyModel
+	{
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(2, ErrorMessage = "Name must be at least 2 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Choose a location from the dropdown")]
+        public string Location { get; set; }
+
+        [Required(ErrorMessage = "Please choose a language from the dropdown")]
+        public string Language { get; set; }
+
+        [MaxLength(20, ErrorMessage = "Comments must be under 20 characters long")]
+        public string? Comment { get; set; }
+    }
+}
+
